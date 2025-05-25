b54bdb728c7132c2fca8c92f863cfc80f7f9c338
gauravkumar20740w <gk5376604@gmail.com>
2025-05-25 16:04:27 +0530

    mongo online done

--- server.js
+++ server.js
@@ -1,83 +1,83 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-const app = express();
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
+// const express = require('express');
+// const { MongoClient } = require('mongodb');
+// const cors = require('cors');
+// const app = express();
+// const uri = "mongodb://localhost:27017";
+// const client = new MongoClient(uri);
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-// Connect MongoDB ek hi baar
-let db;
-client.connect().then(() => {
-  db = client.db("myapp");
-  console.log("MongoDB connected");
-});
+// // Connect MongoDB ek hi baar
+// let db;
+// client.connect().then(() => {
+//   db = client.db("myapp");
+//   console.log("MongoDB connected");
+// });
 
-// Get all users
-app.get('/users', async (req, res) => {
-  try {
-    const users = db.collection("users");
-    const allUsers = await users.find().toArray();
-    res.json(allUsers);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// // Get all users
+// app.get('/users', async (req, res) => {
+//   try {
+//     const users = db.collection("users");
+//     const allUsers = await users.find().toArray();
+//     res.json(allUsers);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
-// Ping route
-app.get('/ping', (req, res) => {
-  res.json({ message: 'Server is working!' });
-});
+// // Ping route
+// app.get('/ping', (req, res) => {
+//   res.json({ message: 'Server is working!' });
+// });
 
-// Root route
-app.get('/', (req, res) => {
-  res.send('Server is running!');
-});
+// // Root route
+// app.get('/', (req, res) => {
+//   res.send('Server is running!');
+// });
 
-// Login route
-app.post('/login', async (req, res) => {
-  try {
-    const { emailOrUsername, password } = req.body;
-    const users = db.collection("users");
-    const user = await users.findOne({
-      $or: [
-        { email: emailOrUsername },
-        { username: emailOrUsername }
-      ],
-      password: password
-    });
-    if (user) {
-      res.json({ success: true, message: 'Login successful', user });
-    } else {
-      res.json({ success: false, message: 'Invalid credentials' });
-    }
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-});
+// // Login route
+// app.post('/login', async (req, res) => {
+//   try {
+//     const { emailOrUsername, password } = req.body;
+//     const users = db.collection("users");
+//     const user = await users.findOne({
+//       $or: [
+//         { email: emailOrUsername },
+//         { username: emailOrUsername }
+//       ],
+//       password: password
+//     });
+//     if (user) {
+//       res.json({ success: true, message: 'Login successful', user });
+//     } else {
+//       res.json({ success: false, message: 'Invalid credentials' });
+//     }
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: err.message });
+//   }
+// });
 
-// Signup route
-app.post('/signup', async (req, res) => {
-  try {
-    const { username, name, email, password } = req.body;
-    const users = db.collection("users");
-    const usernameExists = await users.findOne({ username });
-    const emailExists = await users.findOne({ email });
-    if (usernameExists) {
-      return res.json({ success: false, message: 'Username already exists!' });
-    }
-    if (emailExists) {
-      return res.json({ success: false, message: 'Email already exists!' });
-    }
-    await users.insertOne({ username, name, email, password });
-    res.json({ success: true, message: 'Sign Up successful! Now you can login.' });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-});
+// // Signup route
+// app.post('/signup', async (req, res) => {
+//   try {
+//     const { username, name, email, password } = req.body;
+//     const users = db.collection("users");
+//     const usernameExists = await users.findOne({ username });
+//     const emailExists = await users.findOne({ email });
+//     if (usernameExists) {
+//       return res.json({ success: false, message: 'Username already exists!' });
+//     }
+//     if (emailExists) {
+//       return res.json({ success: false, message: 'Email already exists!' });
+//     }
+//     await users.insertOne({ username, name, email, password });
+//     res.json({ success: true, message: 'Sign Up successful! Now you can login.' });
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: err.message });
+//   }
+// });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server running on http://localhost:3000');
+// });
